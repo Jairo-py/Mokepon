87b971857864d9ff1d88cf885e9bc22f0509c578
Jairo-py <ochoajairo73@gmail.com>
2023-06-19 19:34:50 -0500

    V1.1

--- salida.js
+++ salida.js
@@ -1,10 +1,18 @@
-<script>
-    let siglas = [
-        'FMI' ,
-        'BID' ,
-        ['ONU' , 'UNESCO'],
-        'BIRF'
-    ]
-    console.log(siglas[2][1]);
+var libro = {
+    titulo: 'Las Legiones Malditas',
+    autor: 'Santiago Posterguillo',
+    informacion:function() {return this.titulo + "escrito por" + this.autor; }
+}
 
+
+function calculo(x) { return (x); }
+
+
+
+
+<script>
+    let paises = [['MX, 'CA'], 'ES']
+    consoloe.log(paises[0][0][0]);
 </script>
+
+let nombre = "Jairo"
